bb611dc365c39b2e29719c649a164d66da183e7f
Akmal Dzaki <akmaldzakih@gmail.com>
2025-12-11 13:26:31 +0700

    v0.4

--- assets/scripts/animator.gd
+++ assets/scripts/animator.gd
@@ -9,14 +9,22 @@ func _process(delta):
 		sprite.flip_h = false
 	elif player_ctrl.direction == -1:
 		sprite.flip_h = true 
-		
-	if abs(player_ctrl.velocity.x) > 0.0:
-		animation_player.play("move")
-	else:
-		animation_player.play("idle")
-		
-	if player_ctrl.velocity.y < 0.0:
-		animation_player.play("jump")
-	elif player_ctrl.velocity.y > 0.0:
-		animation_player.play("fall")
-		
+	if player_ctrl.attacking: 
+		animation_player.play("Attack")
+	if !player_ctrl.attacking:
+		if abs(player_ctrl.velocity.x) > 0.0:
+			animation_player.play("move")
+		else:
+			animation_player.play("idle")
+
+		if player_ctrl.velocity.y < 0.0:
+			animation_player.play("jump")
+		elif player_ctrl.velocity.y > 0.0:
+			animation_player.play("fall")
+
+
+
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name == "Attack":
+		player_ctrl.attacking = false
+		player_ctrl.speed = 5
--- assets/scripts/player_ctrl.gd
+++ assets/scripts/player_ctrl.gd
@@ -8,6 +8,7 @@ class_name controller
 var speed_multiplier = 30.0
 var jump_multiplier = -30.0
 var direction = 0
+var attacking = false
 
 #const SPEED = 300.0
 #const JUMP_VELOCITY = -400.0
@@ -15,6 +16,9 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
 	# Add the gravity.
+	if Input.is_action_just_pressed("attack"):
+		attacking = true
+		speed = 3
 	if Input.is_action_pressed("ml"):
 		sprite.scale.x = abs(sprite.scale.x) * -1
 	if Input.is_action_pressed("mr"):
@@ -33,5 +37,4 @@ func _physics_process(delta):
 		velocity.x = direction * speed * speed_multiplier
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed * speed_multiplier)
-
 	move_and_slide()
